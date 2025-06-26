c201375b24e582d2aa4e92eb8e72a3b0ee630e84
Abhishek Kumar Singh <119528437+abhishek02122002@users.noreply.github.com>
2025-06-26 11:02:33 +0530

    Time: 0 ms (100%), Space: 10.3 MB (58.68%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
+        // pahle stack me -1 [ or length calculation]
+        // jab (  ayega to pish krna index 
+        // jab ) ayega to pop krke dekhna ki empty ho gya stack 
+        // agar empty hoga to index push kr dena 
+        // aagr empty nhi hoga to pop krke length nikal lena 
+        // len aur maxlen ko compare krke maxlen ko return kr do
         int maxLen =0 ;
         stack<int> st;
         st.push(-1);
