4fbb99d3297e61e61f3abbd02bd6e78564daa911
Abhishek Kumar Singh <119528437+abhishek02122002@users.noreply.github.com>
2025-08-04 22:23:58 +0530

    Time: 0 ms (100%), Space: 8.2 MB (50.57%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int capitalCount = 0;
+        for (int i = 0; i < word.size(); i++) {
+            if (isupper(word[i])) {
+                capitalCount++;
+            }
+        }
+
+        // sab capital hai
+        if (capitalCount == word.size())
+            return true;
+
+        // ye sab small wala case ho gya
+        if (capitalCount == 0)
+            return true;
+
+        // ab sirf pahla wala capital ho uska logic <-- phle letter ko word[0] se bhi le sakte hai ( array wala rule hai vroo)
+        if (capitalCount == 1 && isupper(word[0]))
+            return true;
+
+        // baki ke sare cases me false return krna hai 
+    return false;
+    }
+};
