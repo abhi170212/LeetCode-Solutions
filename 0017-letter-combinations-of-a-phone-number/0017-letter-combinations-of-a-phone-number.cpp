965b5668df627c11449ecfbd0de57d7349061ade
Abhishek Kumar Singh <119528437+abhi170212@users.noreply.github.com>
2025-09-05 18:31:59 +0530

    Time: 0 ms (100%), Space: 8.9 MB (95.86%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty())
+            return {};
+        vector<string> ans{""};
+        const vector<string> digitToLetters{"", "", "abc", "def", "ghi",
+                                            "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        for (char d : digits) {
+            vector<string> temp;
+            for (const string& s : ans)
+                for (char c : digitToLetters[d - '0'])
+                    temp.push_back(s + c);
+            ans = move(temp);
+        }
+        return ans;
+    }
+};
